f417d225dba0f5d199058bd921adb5a42a8e61e0
Floxylak <floxylak359@gmail.com>
2025-02-19 12:00:27 +0200

    player movement

---
+++ FlapaJam/Assets/Input/PlayerLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float _xRotation;
+    
+    public float xSensitivity = 10f;
+    public float ySensitivity = 10f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ProcessLook(Vector2 input)
+    {
+        if (cam is null)
+            return;
+        
+        var mouseX = input.x * xSensitivity * Time.deltaTime;
+        var mouseY = input.y * ySensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -80f, 80f);
+        
+        cam.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ FlapaJam/Assets/Input/PlayerMotor.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController _controller;
+    private InputManager _inputManager;
+    private Vector3 _playerVelocity;
+    private Vector3 _moveDirection;
+    private bool _isGrounded;
+    private float _originalSpeed;
+    
+    public float speed = 5f;
+    public float gravity = -9.81f * 3f;
+    public float movementInertia = 3f;
+    
+    private float _sprintSpeed;
+    
+    private bool _lerpCrouch;
+    private float _crouchTimer;
+
+    public bool toggleSprint; // If true, sprint is toggled else it's hold
+    public bool toggleCrouch; // If true, crouch is toggled else it's hold
+
+    public Transform playerModel;
+
+    private void Awake()
+    {
+        _inputManager = GetComponent<InputManager>();
+    }
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _originalSpeed = speed;
+    }
+
+    private void Update()
+    {
+        _sprintSpeed = speed * 1.5f;
+    
+        _isGrounded = _controller.isGrounded;
+        HandleCrouchLerp();
+
+        if (_isGrounded && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = -2f; 
+        }
+        else if (!_isGrounded)
+        {
+            _playerVelocity.y += gravity * Time.deltaTime; 
+        }
+
+        _controller.Move((_moveDirection * speed + _playerVelocity) * Time.deltaTime);
+    }
+
+
+    public void ProcessMove(Vector2 input)
+    {
+        if (input.magnitude == 0)
+        {
+            _moveDirection = Vector3.Lerp(_moveDirection, Vector3.zero, movementInertia * Time.deltaTime);
+            return;
+        }
+        
+        var targetDirection = transform.TransformDirection(new Vector3(input.x, 0, input.y));
+        _moveDirection = Vector3.Lerp(_moveDirection, targetDirection, movementInertia * Time.deltaTime);
+    }
+
+    public void Crouch(bool keyDown)
+    {
+        _lerpCrouch = true;
+        _crouchTimer = 0f;
+        speed = keyDown ? _originalSpeed / 2 : _originalSpeed;
+    }
+
+    public void Sprint(bool keyDown)
+    {
+        speed = keyDown ? _sprintSpeed : _originalSpeed;
+    }
+
+    private void HandleCrouchLerp()
+    {
+        if (!_lerpCrouch)
+            return;
+        
+        _crouchTimer += Time.deltaTime;
+        float p = _crouchTimer / 1;
+        p *= p;
+
+        _controller.height = Mathf.Lerp(_controller.height, _inputManager.IsCrouching ? 1 : 2, p);
+
+        if (playerModel is not null)
+        {
+            var targetScale = _inputManager.IsCrouching ? new Vector3(1, 0.5f, 1) : Vector3.one;
+            playerModel.localScale = Vector3.Lerp(playerModel.localScale, targetScale, p);
+        }
+
+        if (p > 1)
+        {
+            _lerpCrouch = false;
+            _crouchTimer = 0f;
+        }
+    }
+}
